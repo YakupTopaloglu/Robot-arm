b02567719a3bd57daa0a0a01816b4920e98d16fc
YakupTopaloglu <112806251+YakupTopaloglu@users.noreply.github.com>
2023-06-19 18:48:03 +0300

    added grids to the screen

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import cv2
 
 # Kamera açma
-cap = cv2.VideoCapture("http://"ip adress" /video")#we have to get camera's ip adress
+cap = cv2.VideoCapture("http://192.168.0.19:8080/video")
 
 while True:
     # Görüntüyü yakalama
     ret, frame = cap.read()
+
+    # Izgara oluşturma
+    height, width, _ = frame.shape
+    grid_size = 50  # Izgara hücre boyutu
+    for x in range(0, width, grid_size):
+        cv2.line(frame, (x, 0), (x, height), (0, 255, 0), 1)  # Dikey çizgiler
+    for y in range(0, height, grid_size):
+        cv2.line(frame, (0, y), (width, y), (0, 255, 0), 1)  # Yatay çizgiler
 
     # Görüntüyü gösterme
     cv2.imshow('IP Kamera', frame)
