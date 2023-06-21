db75ff306ba9b15a3b32272997a54141474724d5
YakupTopaloglu <112806251+YakupTopaloglu@users.noreply.github.com>
2023-06-21 22:05:59 +0300

    Connect arduino's servo with python

---
+++ servoçalıştırma.py
@@ -0,0 +1,24 @@
+from pyfirmata import Arduino, SERVO, util
+from time import sleep
+
+port ='COM6'
+pin=10
+board=Arduino(port)
+
+board.digital[pin].mode=SERVO
+
+def rotatservo(pin,angle):
+    board.digital[pin].write(angle)
+    sleep(0.015)
+
+while True:
+    x=input("input : ")
+    if x=="1":
+        for i in range(0,100):
+            rotatservo(pin,i)
+    elif x=="2":
+        for i in range(0,90):
+            rotatservo(pin,i)
+    elif x=="3":
+        for i in range(0,270):
+            rotatservo(pin,i)
