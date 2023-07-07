b3f7e9a0c72a3c793d2051310afe8e615823e64c
YakupTopaloglu <112806251+YakupTopaloglu@users.noreply.github.com>
2023-07-07 20:51:15 +0300

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -18,10 +18,10 @@ void moveServo(int distance) {
   if (distance == 0) {
     servo.write(90);  // Eğer mesafe 0 ise servo motoru 90 dereceye ayarla
   } else if (distance > 0) {
-    int angle = map(distance, 0, 360, 90, 180);  // Mesafeyi açıya dönüştür
+    int angle = map(distance, 0, 360, 0, 90);  // Mesafeyi açıya dönüştür
     servo.write(angle);
   } else if (distance < 0) {
-    int angle = map(distance, 0, -360, 90, 0);  // Mesafeyi açıya dönüştür
+    int angle = map(distance, 0, -360, 90, 180);  // Mesafeyi açıya dönüştür
     servo.write(angle);
   }
 }
