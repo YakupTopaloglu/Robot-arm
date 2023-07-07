8ac089b6a89d1062699838178707c44c1329b1a1
YakupTopaloglu <112806251+YakupTopaloglu@users.noreply.github.com>
2023-07-07 22:23:54 +0300

    fixed to servo angle for distance values

--- servo.ino
+++ servo.ino
@@ -17,11 +17,9 @@ void loop() {
 void moveServo(int distance) {
   if (distance == 0) {
     servo.write(90);  // Eğer mesafe 0 ise servo motoru 90 dereceye ayarla
-  } else if (distance > 0) {
-    int angle = map(distance, 0, 360, 0, 90);  // Mesafeyi açıya dönüştür
-    servo.write(angle);
-  } else if (distance < 0) {
-    int angle = map(distance, 0, -360, 90, 180);  // Mesafeyi açıya dönüştür
+  } else {
+    int angle = map(distance, 360, -360, 180, 0);  // Mesafeyi açıya dönüştür
     servo.write(angle);
   }
+
 }
